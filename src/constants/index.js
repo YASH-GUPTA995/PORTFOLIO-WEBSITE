392e4e7ebc3b08681da354562e1bd78780f6f467
Yash Gupta <engineeryash0995@gmail.com>
2026-07-16 13:03:57 +0530

    updated messaging system

--- src/constants/index.js
+++ src/constants/index.js
@@ -39,6 +39,13 @@ export const QUICK_FACTS = [
 //   publicKey: 'MPwANOH-KNtzjOWJd',
 // }
 
+export const EMAILJS_CONFIG = {
+  serviceId: import.meta.env.VITE_EMAILJS_SERVICE_ID,
+  templateId: import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+  publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
+}
+
+
 export const PROFILE = {
   name: 'Yash Gupta',
   title: 'Software Engineer',
